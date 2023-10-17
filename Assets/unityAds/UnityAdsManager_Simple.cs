b9c276c09030b14446264f1ad9806669e7da9a61
hsson0822 <hsson0822@naver.com>
2023-10-17 20:33:45 +0900

    [추가] 광고 추가

---
+++ Assets/unityAds/UnityAdsManager_Simple.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Advertisements;
+
+public class UnityAdsManager_Simple : MonoBehaviour
+{
+    static UnityAdsManager_Simple instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+        instance = this;
+    }
+
+    public static UnityAdsManager_Simple Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    public void ShowAd()
+	{
+		if (Advertisement.IsReady())
+		{
+			Advertisement.Show();
+		}
+	}
+}
