0900efe9afecd0117ba04b0b787d991f56927f86
hsson0822 <hsson0822@naver.com>
2023-10-17 20:22:47 +0900

    [추가] 사운드 추가

---
+++ Assets/Audio/AudioManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour {
+
+    static AudioManager instance;
+
+    public static AudioManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    public AudioSource click;
+    public AudioSource coin;
+    public AudioSource bullet;
+    public AudioSource bomb;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        
+    }
+
+    public void ClickSoundPlay()
+    {
+        click.Play();
+    }
+
+    public void CoinSoundPlay()
+    {
+        coin.Play();
+    }
+    public void BulletSoundPlay()
+    {
+        bullet.Play();
+    }
+    public void BombSoundPlay()
+    {
+        bomb.Play();
+    }
+}
