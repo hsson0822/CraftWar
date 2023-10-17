e35b29d428b3ae493b57c4546959b694b814bcfc
hsson0822 <hsson0822@naver.com>
2023-10-17 20:34:14 +0900

    [추가] 프로젝트 추가

---
+++ Assets/InApp/IAPManager.cs
@@ -0,0 +1,183 @@
+﻿#if UNITY_ANDROID || UNITY_IPHONE
+//#define RECEIPT_VALIDATION
+#endif
+
+using System;
+using UnityEngine;
+using UnityEngine.Purchasing;
+using UnityEngine.UI;
+using UnityEngine.Analytics;
+
+#if RECEIPT_VALIDATION
+using UnityEngine.Purchasing.Security;
+#endif
+
+public class IAPManager : MonoBehaviour, IStoreListener
+{
+
+    public static IAPManager instance;
+
+	private static IStoreController storeController;
+	private static IExtensionProvider extensionProvider;
+	public string[] productIds;
+
+    void PushReward(int index)
+    {
+        switch (index)
+        {
+            case 0:
+				PlayerPrefs.SetInt("Gold", PlayerPrefs.GetInt("Gold") + 10000);
+				StoreManager.GetInstance().UpdateGold();
+                break;
+
+            default:
+                Debug.Log("Index Error");
+			break;
+		}
+	}
+
+
+	void Success(string productId) {
+		for (int i = 0; i < productIds.Length; i++) {
+			if (string.Compare (productId, productIds [i],false) == 0) {
+				PushReward (i);
+			}
+		}
+	}
+
+	void Fail() {
+		// Fail
+	}
+
+	void Awake() {
+		instance = this;
+		DontDestroyOnLoad (gameObject);
+	}
+	void Start()
+	{
+		InitializePurchasing();
+	}
+
+	private bool IsInitialized()
+	{
+		return (storeController != null && extensionProvider != null);
+	}
+
+	public void InitializePurchasing()
+	{
+		if (IsInitialized())
+			return;
+
+		var module = StandardPurchasingModule.Instance();
+
+		ConfigurationBuilder builder = ConfigurationBuilder.Instance(module);
+		foreach (string id in productIds) {
+			builder.AddProduct(id, ProductType.Consumable, new IDs
+				{
+					{ id, AppleAppStore.Name },
+					{ id, GooglePlay.Name },
+				});
+		}
+		UnityPurchasing.Initialize(this, builder);
+	}
+	public void BuyProductInt(int type) {
+		BuyProductID (productIds [type]);
+	}
+	public void BuyProductID(string productId)
+	{
+		try
+		{
+			if (IsInitialized())
+			{
+				Product p = storeController.products.WithID(productId);
+				if (p != null && p.availableToPurchase)
+				{
+					storeController.InitiatePurchase(p);
+				}
+				else
+				{
+					Debug.Log("구매 실패 #1");
+				}
+			}
+			else
+			{
+				Debug.Log("구매 실패 #2");
+			}
+		}
+		catch (Exception e)
+		{
+			Debug.Log("구매 실패 #" + e);
+		}
+	}
+
+	public void RestorePurchase()
+	{
+		if (!IsInitialized())
+		{
+			Debug.Log ("초기화 실패");
+			return;
+		}
+
+		if (Application.platform == RuntimePlatform.IPhonePlayer || Application.platform == RuntimePlatform.OSXPlayer)
+		{
+			var apple = extensionProvider.GetExtension<IAppleExtensions>();
+
+			apple.RestoreTransactions
+			(
+				(result) => { Debug.Log("RestorePurchases continuing: " + result + ". If no further messages, no purchases available to restore."); }
+			);
+		}
+		else
+		{
+			Debug.Log ("현재 플랫폼은 지원하지 않습니다.");
+		}
+	}
+
+	public void OnInitialized(IStoreController sc, IExtensionProvider ep)
+	{
+		storeController = sc;
+		extensionProvider = ep;
+
+		StoreManager.GetInstance().SetGoldText(storeController.products.WithID("Gold").metadata.localizedPriceString);
+    }
+
+    public void OnInitializeFailed(InitializationFailureReason reason)
+	{
+		Debug.Log ("초기화 실패 #" + reason);
+	}
+	public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs args)
+	{
+		bool validPurchase = true;
+		#if RECEIPT_VALIDATION
+		var validator = new CrossPlatformValidator (GooglePlayTangle.Data (), AppleTangle.Data (), 
+			                UnityChannelTangle.Data (), Application.identifier);
+		try {
+			var result = validator.Validate (args.purchasedProduct.receipt);
+			Debug.Log ("Receipt is valid. Contents:");
+			foreach (IPurchaseReceipt productReceipt in result) {
+				Debug.Log (productReceipt.productID);
+				Debug.Log (productReceipt.purchaseDate);
+				Debug.Log (productReceipt.transactionID);
+			}
+		} catch (IAPSecurityException) {
+			Debug.Log ("Invalid receipt, not unlocking content");
+			validPurchase = false;
+		}
+		#endif
+		if (validPurchase) {
+			foreach (string id in productIds) {
+				if (args.purchasedProduct.definition.id.Equals (id)) {
+					Success (id);
+				}
+			}
+		}
+		return PurchaseProcessingResult.Complete;
+	}
+
+	public void OnPurchaseFailed(Product product, PurchaseFailureReason failureReason)
+	{
+		Debug.Log(string.Format("OnPurchaseFailed: FAIL. Product: '{0}', PurchaseFailureReason: {1}", product.definition.storeSpecificId, failureReason));
+	}
+
+    
+}
---
+++ Assets/Localize/LocalizeManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalizeManager : MonoBehaviour {
+	public string textDataPath = "LanguageData";
+	public SystemLanguage baseLanguage;
+	public SystemLanguage[] useLanguages;
+	private SystemLanguage systemLanguage;
+
+	public Dictionary<SystemLanguage, List<string>> textDic = new Dictionary<SystemLanguage, List<string>> ();
+
+	public static LocalizeManager Instance {
+		get {
+			return instance;
+		}
+	}
+	private static LocalizeManager instance;
+
+    void Awake()
+    {
+        if (instance)
+        {
+            DestroyImmediate(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        SystemLanguage deviceSystemLanguage = Application.systemLanguage;
+        switch (deviceSystemLanguage)
+        {
+
+            case SystemLanguage.Korean:
+                systemLanguage = deviceSystemLanguage;
+                break;
+            default:
+                systemLanguage = baseLanguage;
+                break;
+        }
+
+        foreach (SystemLanguage tLanguage in useLanguages)
+        {
+            textDic.Add(tLanguage, new List<string>());
+        }
+
+        TextAsset ta = Resources.Load<TextAsset>(textDataPath);
+        string[] lines = ta.text.Split('\n');
+        foreach (string line in lines)
+        {
+            string[] words = line.Split('\t');
+            for (int i = 0; i < useLanguages.Length; i++)
+            {
+                textDic[useLanguages[i]].Add(words[i]);
+
+            }
+        }
+    }
+	public static string GetText(int index) {
+		if (Instance == null) {
+			Debug.LogError ("Instance Null");
+			return string.Empty;
+		}
+		if (index < 0 || index >= Instance.textDic [Instance.systemLanguage].Count) {
+			Debug.LogError ("Wrong Index : " + index);
+			return string.Empty;
+		}
+		return Instance.textDic [Instance.systemLanguage] [index];
+	}
+}
